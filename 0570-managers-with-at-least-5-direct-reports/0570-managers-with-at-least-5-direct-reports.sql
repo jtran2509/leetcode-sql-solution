500558d9cf732f716b35700e2b79c45c12349066
Dung (Jade) Tran <85411194+jtran2509@users.noreply.github.com>
2026-04-15 19:02:00 -0400

    Time: 349 ms (39.99%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -2,4 +2,4 @@
 SELECT e1.name FROM Employee e1
 JOIN Employee e2 ON e1.id = e2.managerId
 GROUP BY e1.name, e1.id # case when people have the same name
-HAVING COUNT(*) >=5
+HAVING COUNT(*) >=5 # Use Count to include everything, even null values
