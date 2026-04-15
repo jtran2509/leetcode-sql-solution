8bcd8c4eaa0ae5f7fce0f9d8ee3614d9ca615231
Dung (Jade) Tran <85411194+jtran2509@users.noreply.github.com>
2026-04-15 19:01:29 -0400

    Time: 352 ms (36.68%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT e1.name FROM Employee e1
+JOIN Employee e2 ON e1.id = e2.managerId
+GROUP BY e1.name, e1.id # case when people have the same name
+HAVING COUNT(*) >=5
